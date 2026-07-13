1236aea1f862e59040403a8b3c9ba99a305d2ded
kunalvanjani705-tech <kunalvanjani705@gmail.com>
2026-07-13 17:56:40 +0530

    Silver Changes

--- Layers/Silver/Transformation.sql
+++ Layers/Silver/Transformation.sql
@@ -1,6 +1,30 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
+===============================================================================
+*/
 
 --Standerdizing crm.cust_info table and loading it into silver layer 
+print '-------------------------------------'
+print '>>>> Truncating Table silver.crm_cust_info <<<<'
+print '-------------------------------------'
+Truncate Table silver.crm_cust_info
 
+PRINT '>> Inserting Data Into: silver.crm_cust_info';
 Insert into silver.crm_cust_info (
     cst_id,
     cst_key,
@@ -34,4 +58,39 @@ from(
     where cst_id is Not Null
 )t 
 where flag_last = 1
+
+--Standerdizing the crm_prd_info table and loading it into silver layer 
+
+print '-------------------------------------'
+print '>>>> Truncating Table Silver.crm_prd_info <<<<'
+print '-------------------------------------'
+Truncate Table Silver.crm_prd_info
+
+PRINT '>> Inserting Data Into: Silver.crm_prd_info';
+insert into Silver.crm_prd_info(
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_name,
+    prd_cost,
+    prd_line,
+    prd_start_date,
+    prd_end_date
+)
+select 
+    prd_id,
+    replace(SUBSTRING(prd_key, 1,5),'-','_') as cat_id,
+    SUBSTRING(prd_key,7, LEN(prd_key)) as prd_key ,
+    prd_name,
+    isnull(prd_cost, 0) as prd_cost,
+case upper(trim(prd_line))
+    when 'M' then 'Mountain'
+    when 'R' then 'Road'
+    when 'T' then 'Touring'
+    when 'S' then 'Other Sales'
+    else 'Unknown'
+end as prd_line,
+    prd_start_date ,
+    DATEADD(Day, -1, lead(prd_start_date) Over(PARTITION BY prd_key Order by prd_start_date))  as prd_end_date
+from bronze.crm_prd_info
 
